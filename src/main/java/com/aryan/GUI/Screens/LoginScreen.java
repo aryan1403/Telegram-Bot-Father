9a78b953d9e092fa49776a3edf5e440978d5b728
Aaryan <aaryan14032006@gmail.com>
2021-07-28 22:27:15 +0530

    Updated login page

--- src/main/java/com/aryan/GUI/Screens/LoginScreen.java
+++ src/main/java/com/aryan/GUI/Screens/LoginScreen.java
@@ -1,9 +1,12 @@
 // By Aaryan(https://t.me/)
 package com.aryan.GUI.Screens;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 
 public class LoginScreen extends JPanel {
     /*
@@ -16,16 +19,23 @@ public class LoginScreen extends JPanel {
     private final int FRAME_WIDTH = 800;
     private final int FRAME_HEIGHT = 600;
 
+    private final Image loginImage = new ImageIcon("src/main/java/com/aryan/GUI/res/loginpage.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+
     // Initialize all the components
     public void __init__(){
         frame = new JFrame();
         
     }
 
-    public void setLoginScreen(){
-        this.setBounds(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
-        this.setBackground(Color.black);
-        this.setLayout(null);
+    public void setLoginScreen(){   
+        this.setBounds(0, 30, FRAME_WIDTH, FRAME_HEIGHT);
+        this.setBackground(Color.white);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(loginImage, 0, 0, this);
+        
     }
     
     public LoginScreen(){
@@ -33,9 +43,11 @@ public class LoginScreen extends JPanel {
         setLoginScreen();
 
         frame.setDefaultCloseOperation(3);
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setLocationRelativeTo(null);
+        
         frame.add(this);
-        frame.pack();
+        frame.setLayout(null);
         frame.setVisible(true);
     }
     
