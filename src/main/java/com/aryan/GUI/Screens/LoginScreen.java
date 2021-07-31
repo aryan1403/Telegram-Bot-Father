ab44062852eb47eef0dfc7dd5ca717efab89968b
Aaryan <aaryan14032006@gmail.com>
2021-07-31 21:24:34 +0530

    Updated Login

--- src/main/java/com/aryan/GUI/Screens/LoginScreen.java
+++ src/main/java/com/aryan/GUI/Screens/LoginScreen.java
@@ -10,10 +10,11 @@ import javax.swing.JTextField;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginScreen extends JPanel {
+public class LoginScreen extends JPanel implements ActionListener {
     /*
      * Login Screen
      * Input :- (Bot Token)
@@ -26,7 +27,7 @@ public class LoginScreen extends JPanel {
 
     private final Image loginImage = new ImageIcon("src/main/java/com/aryan/GUI/res/loginpage.jpg").getImage().getScaledInstance(530, 550, Image.SCALE_SMOOTH);
 
-    JTextField herokuApiKeyField = new JTextField();
+    JTextField botTokenField = new JTextField();
 
     JButton loginButton = new JButton();
 
@@ -37,24 +38,30 @@ public class LoginScreen extends JPanel {
     }
 
     public void setLoginScreen(){   
-        herokuApiKeyField.setBounds(FRAME_WIDTH/3, FRAME_HEIGHT/2 - FRAME_HEIGHT/5, FRAME_WIDTH - (FRAME_WIDTH/2 - FRAME_WIDTH/8) - 80, 30);
-        herokuApiKeyField.setBackground(Color.white);
-        herokuApiKeyField.setFont(new Font("Aerial", Font.PLAIN, 18));
-        herokuApiKeyField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.black));
+        // Bot token TextField Attributes
+        botTokenField.setBounds(FRAME_WIDTH/3, FRAME_HEIGHT/2 - FRAME_HEIGHT/5, FRAME_WIDTH - (FRAME_WIDTH/2 - FRAME_WIDTH/8) - 80, 30);
+        botTokenField.setBackground(Color.white);
+        botTokenField.setFont(new Font("Aerial", Font.PLAIN, 18));
+        botTokenField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.black));
 
-        loginButton.setBounds(FRAME_WIDTH/2 + FRAME_WIDTH/5, FRAME_HEIGHT - FRAME_HEIGHT/4, 100, 50);
+        // LoginButton Attributes
+        loginButton.setBounds(FRAME_WIDTH/2 + FRAME_WIDTH/4 + 10, FRAME_HEIGHT - FRAME_HEIGHT/4, 100, 50);
         loginButton.setText("Login");
+        loginButton.setForeground(Color.red);
+        loginButton.setFont(new Font("Comic Sans", Font.BOLD, 22));
+        loginButton.setBorder(BorderFactory.createMatteBorder(3, 0, 0, 2, Color.black));
         loginButton.setOpaque(false);
         loginButton.setContentAreaFilled(false);
-        loginButton.setBorderPainted(false);
 
+        // Frame Attributes
         this.setBounds(0, 15, FRAME_WIDTH, FRAME_HEIGHT);
         this.setBackground(Color.white);
         this.setLayout(null);
-        this.add(herokuApiKeyField);
+        this.add(botTokenField);
         this.add(loginButton);
     }
 
+    // Paint/Draw Image & String to the panel
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.drawImage(loginImage, 0, 0, this);
@@ -63,10 +70,11 @@ public class LoginScreen extends JPanel {
         g.setFont(new Font("MV Boli", Font.BOLD+ Font.ITALIC, 35));
         g.drawString("Login Page", FRAME_WIDTH/2 - FRAME_WIDTH/12 - g.getFont().getSize(), FRAME_HEIGHT/2 - FRAME_HEIGHT/3 - g.getFont().getSize());
         
-        g.setFont(new Font("Aerial", Font.ITALIC, 22));
-        g.drawString("Heroku API Key :- ", 50, FRAME_HEIGHT/2 - FRAME_HEIGHT/5 + 20);
+        g.setFont(new Font("Aerial", Font.ITALIC + Font.BOLD, 25));
+        g.drawString("Bot Token ", 100, FRAME_HEIGHT/2 - FRAME_HEIGHT/5 + 20);
     }
     
+    // Constructor
     public LoginScreen(){
         __init__();
         setLoginScreen();
@@ -79,6 +87,14 @@ public class LoginScreen extends JPanel {
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setBackground(Color.white);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == loginButton){
+            
+        }
+        
     }
     
 }
