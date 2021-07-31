c57c5b565bb9806f5be2ca7fb839b136fed1b735
Aaryan <aaryan14032006@gmail.com>
2021-07-31 21:59:46 +0530

    Updated

--- src/main/java/com/aryan/configuration/config.java
+++ src/main/java/com/aryan/configuration/config.java
@@ -8,4 +8,6 @@ public class config {
     private static final Dotenv dotenv = new DotenvBuilder().ignoreIfMissing().load();
 
     public static String botToken = dotenv.get("bottoken");
+    public static String botUsername = dotenv.get("botusername");
+    public static String handler = dotenv.get("handler");
 }
